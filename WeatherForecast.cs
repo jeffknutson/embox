b67a1ffa42b1bf880572e5bc504a330799851620
Jeff Knutson <jeff@goodguyconsulting.com>
2020-12-20 14:11:53 -0600

    added a new property to test the ci/cd pipeline

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,14 +2,26 @@ using System;
 
 namespace embox
 {
-    public class WeatherForecast
+  public class WeatherForecast
+  {
+    public WeatherForecast()
     {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+      var seed = DateTime.Now.Ticks;
+      Pancakes = new Random(Convert.ToInt32(seed)).Next();
     }
+
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string Summary { get; set; }
+
+    public int Pancakes
+    {
+      get; set;
+    }
+  }
 }
